6d6f849aae1bc9d04b644f511e48564cd8d8d3a6
NICOLExALV <a21490558@itmexicali.edu.mx>
2024-12-08 20:34:16 -0800

    version2

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const port = process.env.port||3000;
-const dbhost = process.env.dbhost||" ";
-const dbuser = process.env.user||" ";
-const dbpassword = process.env.password||" ";
-const name = process.env.name||" ";
-const portdb = process.env.dbport||" ";
+const dbhost = process.env.dbhost||"junction.proxy.rlwy.net";
+const dbuser = process.env.user||"root";
+const dbpassword = process.env.password||"vEHNtIOAVbxLECFwKdYfjaSTjVEJAmtv";
+const name = process.env.name||"railway";
+const portdb = process.env.dbport||"16907";
 
 module.exports = {
     port,
@@ -13,5 +13,3 @@ module.exports = {
     name,
     portdb
 };
-
-
